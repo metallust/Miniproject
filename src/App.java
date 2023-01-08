78055a727f2d84fd436c37cd563229739664c260
Irfan <shaikh.irfan.oct16@gmail.com>
2023-01-08 20:24:17 +0530

    added game 2048

--- src/App.java
+++ src/App.java
@@ -1,11 +1,20 @@
 public class App {
     public static void main(String[] args) {
 
-        LoginForm loginForm = new LoginForm();
-        loginForm.initialize();
+        // LoginForm loginForm = new LoginForm();
+        // loginForm.initialize();
 
         // SignUp signUp = new SignUp();
         // signUp.initialize();
+
+        new Game2048();
+
+        // TicTacToe ticTacToe = new TicTacToe();
+
+        // Game game = new Game();
+
+        // new Game();
+
     }
 
 }
